50236abd2d0e67e061693187fc0263fc6d40b852
jmontecinos <jmontecinos@ofbox.com>
2022-11-16 21:34:58 -0600

    Implementacion problema 1

--- 03 - Dynamic Programming/05_max_1D_range_sum_dpfull.cpp
+++ 03 - Dynamic Programming/05_max_1D_range_sum_dpfull.cpp
@@ -1,29 +1,37 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 
 using namespace std;
 
-typedef struct{
-    int max_sum;
+typedef struct
+{
+    float max_sum;
     string max_range;
 } Answer;
 
-Answer max_1d_range_sum(int* A, int n){
-    int M[n+1], S = 0;
-    string R[n+1], cad = "";
+Answer max_1d_range_sum(float *A, int *Min, int n)
+{
+    float M[n + 1], S = 0.0;
+    string R[n + 1], cad = "";
 
     M[0] = 0;
     R[0] = "";
-    for(int i = 1; i <= n; i++){
-        S += A[i-1];
+    for (int i = 1; i <= n; i++)
+    {
+        S += A[i - 1];
 
-        if( S > A[i-1] ) cad += "-"+to_string(i-1);
-        else cad = to_string(i-1);
-        S = max(S, A[i-1]);
+        if (S > A[i - 1])
+            cad += "-" + to_string(Min[i - 1]);
+        else
+            cad = to_string(Min[i - 1]);
+        S = max(S, A[i - 1]);
 
-        if( S > M[i-1] ) R[i] = cad;
-        else R[i] = R[i-1];
-        M[i] = max(S, M[i-1]);
+        if (S > M[i - 1])
+            R[i] = cad;
+        else
+            R[i] = R[i - 1];
+        M[i] = max(S, M[i - 1]);
     }
 
     Answer res;
@@ -32,13 +40,43 @@ Answer max_1d_range_sum(int* A, int n){
     return res;
 }
 
-int main(void){
-    int n = 8;
-    int A[n] = {-2,1,-3,4,-1,2,1,-5,4};
+int main(void)
+{
+    /**Declaracion de variables auxiliares
+     * int n: longitud de registros
+     * int dato: auxiliar para guardar un dato leido
+     */
 
-    Answer respuesta = max_1d_range_sum(A, n);
+    int n, registros;
 
-    cout << "El resultado máximo es: " << respuesta.max_sum << ", y ocurre en el rango: " << respuesta.max_range << ".\n";
+    ifstream file;
+    file.open("espatulas.txt");
 
+    // Comprobamos primero que el archivo se pudo leer
+    if (!file)
+    {
+        cout << "Can't open input file.\n";
+        return 1;
+    }
+    
+    // Usamos un while loop para recorrer el archivo
+    while (file >> registros)
+    {
+        n = registros;   // Guardamos la cantidad de registros de la tienda
+        int Min[n]; // Arreglo para guardar rango de minutos de venta
+        float A[n]; // Arreglo para guardar ganancia por venta
+        for (int i = 0; i < n; i++)
+        {
+            file >> Min[i];
+            file >> A[i];
+        }
+
+        Answer respuesta = max_1d_range_sum(A, Min, n);
+
+        cout << "El resultado máximo es: " << respuesta.max_sum << ", y ocurre en el rango: " << respuesta.max_range << ".\n";
+    }
+
+    // Close the file
+    file.close();
     return 0;
 }
--- 03 - Dynamic Programming/08_knapsack_dpfull.cpp
+++ 03 - Dynamic Programming/08_knapsack_dpfull.cpp
@@ -3,33 +3,40 @@
 
 using namespace std;
 
-typedef struct{
+typedef struct
+{
     int max_value;
     string max_combi;
 } Answer;
 
-Answer knapsack(int K, int* V, int* W, int n){
-    int M[n+1][K+1];
-    string S[n+1][K+1];
+Answer knapsack(int K, int *V, int *W, int n)
+{
+    int M[n + 1][K + 1];
+    string S[n + 1][K + 1];
 
-    for(int i = 0; i <= n; i++){
+    for (int i = 0; i <= n; i++)
+    {
         M[i][0] = 0;
         S[i][0] = "";
     }
 
-    for(int j = 1; j <= K; j++){
+    for (int j = 1; j <= K; j++)
+    {
         M[0][j] = 0;
         S[0][j] = "";
     }
 
-    for(int i = 1; i <= n; i++)
-        for(int j = 1; j <= K; j++)
-            if( j >= W[i-1] && ( V[i-1] + M[i-1][j-W[i-1]] > M[i-1][j] ) ){
-                M[i][j] = V[i-1] + M[i-1][j-W[i-1]];
-                S[i][j] = to_string(i) + "," + S[i-1][j-W[i-1]];
-            }else{
-                M[i][j] = M[i-1][j];
-                S[i][j] = S[i-1][j];
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= K; j++)
+            if (j >= W[i - 1] && (V[i - 1] + M[i - 1][j - W[i - 1]] > M[i - 1][j]))
+            {
+                M[i][j] = V[i - 1] + M[i - 1][j - W[i - 1]];
+                S[i][j] = to_string(i) + "," + S[i - 1][j - W[i - 1]];
+            }
+            else
+            {
+                M[i][j] = M[i - 1][j];
+                S[i][j] = S[i - 1][j];
             }
 
     Answer res;
@@ -38,9 +45,11 @@ Answer knapsack(int K, int* V, int* W, int n){
     return res;
 }
 
-int main(void){
-    int n = 3, K = 50;
-    int V[n] = {60,100,120}, W[n] = {10,20,30};
+int main(void)
+{
+    int dato;
+    int n, K;
+    int V[n] = {60, 100, 120}, W[n] = {10, 20, 30};
 
     Answer respuesta = knapsack(K, V, W, n);
 
